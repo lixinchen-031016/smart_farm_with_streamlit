dfa0ac2769dcb01d86fc8758fe7c8a7aee2c66ba
lixinchen031016 <1677343841@qq.com>
2025-02-09 19:45:09 +0800

    docs(使用说明书): 更新数据库初始化脚本和使用说明
    - 更新 init.sql 文件，添加详细的表结构创建语句
    - 修改使用说明书中的 Docker 镜像源配置步骤，使用 vim 编辑器代替 tee 命令

--- init.sql
+++ init.sql
@@ -1,1 +1,66 @@
-CREATE DATABASE IF NOT EXISTS intelligent_farm;
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : root
+ Source Server Type    : MySQL
+ Source Server Version : 80300
+ Source Host           : localhost:3306
+ Source Schema         : intelligent_farm
+
+ Target Server Type    : MySQL
+ Target Server Version : 80300
+ File Encoding         : 65001
+
+ Date: 09/02/2025 19:25:48
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for intelligent_farm_airtemperaturehumidity
+-- ----------------------------
+DROP TABLE IF EXISTS `intelligent_farm_airtemperaturehumidity`;
+CREATE TABLE `intelligent_farm_airtemperaturehumidity`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `temperature` double NOT NULL,
+  `humidity` double NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 660 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for intelligent_farm_soilmoisture
+-- ----------------------------
+DROP TABLE IF EXISTS `intelligent_farm_soilmoisture`;
+CREATE TABLE `intelligent_farm_soilmoisture`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `value` double NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 660 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for intelligent_farm_soilnutrient
+-- ----------------------------
+DROP TABLE IF EXISTS `intelligent_farm_soilnutrient`;
+CREATE TABLE `intelligent_farm_soilnutrient`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `value` double NOT NULL,
+  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 660 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `last_login_time` timestamp NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
